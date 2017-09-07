ac3a0e519f6f624bfdf8c3402ae1cba3b2be6d80
Felipe <felipejmesina@gmail.com>
2017-09-06 18:58:59 -0700

    completed assignment

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+aboutMe = {
+    "name" : "Felipe",
+    "age"  : 29,
+    "country" : "Mexico",
+    "language" : "JavaScript"
+}
+
+print "My name is " + aboutMe["name"] + " \nMy age is " + str(aboutMe["age"]) + "\nMy country of birth is " + aboutMe["country"] + "\nMy favorite language is " + aboutMe["language"] 
